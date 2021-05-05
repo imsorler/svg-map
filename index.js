d6c2a696588c58386f491c9370bccd37e76ae398
imsorler <imsorler@gmail.com>
2021-05-05 20:53:24 +0300

    added geolication and notification

--- index.js
+++ index.js
@@ -89,3 +89,49 @@ mapLinks.forEach((elem) => {
     currentElement.classList.remove('active')
   })
 })
+
+const infoUser = document.getElementById('userInfo')
+
+if ('geolocation' in navigator) {
+  navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position)
+    const p = document.createElement('p')
+    p.classList.add('position')
+    p.textContent = `широта: ${position.coords.latitude}, долгота: ${position.coords.longitude}`
+    infoUser.append(p)
+  })
+
+  const watch = navigator.geolocation.watchPosition((newPosition) => {
+    const p = document.querySelector('.position')
+    p.textContent = `широта: ${newPosition.coords.latitude}, долгота: ${newPosition.coords.longitude}`
+  })
+
+  setTimeout(() => {
+    navigator.geolocation.clearWatch(watch)
+    showNotification()
+  }, 10 * 1000)
+} else {
+  console.log('geolocation api не предоставляется вашим браузером')
+}
+
+function showNotification() {
+  const notification = new Notification('уведомление об отключении слежки', {
+    body: 'мы больше за вами не следим!',
+    icon: 'https://image.shutterstock.com/image-vector/outline-location-icon-illustration-vector-600w-1039285465.jpg'
+  })
+
+  notification.onclick = (e) => {
+    window.location.href = 'https://google.com'
+  }
+}
+
+if (Notification.permission === 'granted') {
+  showNotification()
+} else if (Notification.permission !== 'denied') {
+  Notification.requestPermission()
+    .then((permission) => {
+      if (permission === 'granted') {
+        showNotification()
+      }
+    })
+}
